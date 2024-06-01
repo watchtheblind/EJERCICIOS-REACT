3d26704309cb73cce9571b4afd71b69bb5e6a9dd
Luis Monasterios <ljmm2k2@gmail.com>
2024-06-01 12:31:10 -0400

    eslint-prettier configs

---
+++ counter/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'airbnb-typescript/base',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    indent: 'error',
+    quotes: ['error', 'single'],
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
